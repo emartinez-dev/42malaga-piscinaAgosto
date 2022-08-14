9862c99370f3c9003b0e0c1d7d2bffc00f481895
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-14 12:00:57 +0200

    Más cambios al ej 7 pero sigue sin funcionar

--- piscina/c_00/ex07/ft_putnbr.c
+++ piscina/c_00/ex07/ft_putnbr.c
@@ -6,20 +6,11 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 11:05:18 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/13 21:12:59 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/14 12:00:35 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_first_digit(int x)
-{
-	while (x >= 10){
-		x = x / 10;
-	}
-	x += 48;
-	write(1, &x, 1);
-}
 
 void	ft_putnbr(int nb)
 {
@@ -29,22 +20,22 @@ void	ft_putnbr(int nb)
 	{
 		write(1, "-", 1);
 		nb *= -1;
-		ft_putnbr(nb);
 	}
-	if (nb < 10)
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb / 10;
+	}
+	else if (nb < 10 && nb >= 0)
 	{
 		c = nb + 48;
 		write(1, &c, 1);
 	}
-	if (nb >= 10)
-	{
-		ft_first_digit(nb);
-		ft_putnbr(nb % 10);
-	}
+
 }
 
 int	main(void)
 {
-	ft_putnbr(100);
+	ft_putnbr(7823452);
 	return (0);
 }
