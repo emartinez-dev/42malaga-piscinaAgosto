c1a184b15217516cf4d8b4476e0ee0f717bc2b73
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-14 20:41:19 +0200

    c_00/ex07 terminado

--- piscina/c_00/ex07/ft_putnbr.c
+++ piscina/c_00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 11:05:18 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/14 12:00:35 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/14 20:37:03 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,14 @@ void	ft_putnbr(int nb)
 		write(1, "-", 1);
 		nb *= -1;
 	}
-	else if (nb >= 10)
-	{
+	if (nb / 10 > 0)
 		ft_putnbr(nb / 10);
-		nb = nb / 10;
-	}
-	else if (nb < 10 && nb >= 0)
-	{
-		c = nb + 48;
-		write(1, &c, 1);
-	}
-
+	c = nb % 10 + 48;
+	write(1, &c, 1);
 }
 
 int	main(void)
 {
-	ft_putnbr(7823452);
+	ft_putnbr(42);
 	return (0);
 }
