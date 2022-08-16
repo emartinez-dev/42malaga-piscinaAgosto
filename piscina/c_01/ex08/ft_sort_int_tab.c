8fbbfddf25039dc66b9c2288766e1f3cae2322dc
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-16 08:20:24 +0200

    Ejercicio 08 terminado

--- piscina/c_01/ex08/ft_sort_int_tab.c
+++ piscina/c_01/ex08/ft_sort_int_tab.c
@@ -6,16 +6,15 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 10:07:22 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/15 12:41:37 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/16 08:19:36 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_check_sort(int *tab, int size){
+int	ft_check_sort(int *tab, int size)
+{
 	while (size > 1)
 	{
-		if (*tab > *(tab+1))
+		if (*tab > *(tab + 1))
 			return (0);
 		size--;
 		tab++;
@@ -34,52 +33,22 @@ void	ft_swap(int *a, int *b)
 
 void	ft_sort_int_tab(int *tab, int size)
 {
-	int *smallest;
-	int i;
+	int	i;
+	int	j;
 
-	i = 1;
-	while(ft_check_sort(tab, size) == 0)
+	i = 0;
+	while (ft_check_sort(tab, size) == 0)
 	{
-		while (i <= size)
+		while (i < size - 1)
 		{
-			smallest = tab + 1;
-			while (*tab >= *smallest)
+			j = 0;
+			while (j < size - i - 1)
 			{
-				if (*smallest <= *tab)
-					ft_swap(tab, smallest);
-				else
-					tab++;
+				if (*(tab + j) > *(tab + j + 1))
+					ft_swap(tab + j, tab + j + 1);
+				j++;
 			}
-		}
 		i++;
+		}
 	}
-}
-
-
-int	main(void)
-{
-	int c[12];
-	int *c1;
-	int loop;
-
-	c[0] = 1;
-	c[1] = 2;
-	c[2] = 3;
-	c[3] = 5;
-	c[4] = 4;
-	c[5] = 6;
-	c[6] = 7;
-	c[7] = 8;
-	c[8] = 9;
-	c[9] = 10;
-	c[10] = 11;
-	c[11] = 12;
-	c1 = &c[0];
-	for (loop = 0; loop < 12; loop++)
-		printf("%d ", c[loop]);
-	ft_sort_int_tab(c1, 12);
-	printf("\n");
-	for (loop = 0; loop < 12; loop++)
-		printf("%d ", c[loop]);
-	return (0);
 }
