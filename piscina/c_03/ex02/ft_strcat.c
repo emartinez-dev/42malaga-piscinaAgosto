61c8abd26992e3bd77f1a17d558cb5d9847ac0df
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-17 18:36:36 +0200

    Ejercicios 2 y 3 C_03

---
+++ piscina/c_03/ex02/ft_strcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 17:52:58 by franmart          #+#    #+#             */
+/*   Updated: 2022/08/17 18:25:35 by franmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	unsigned int	space_needed;
+	unsigned int	dest_size;
+	unsigned int	i;
+
+	dest_size = ft_strlen(dest);
+	space_needed = dest_size + ft_strlen(src) + 1;
+	i = 0;
+	if (space_needed < (sizeof(*dest) / sizeof(dest[0])))
+	{
+		while (src[i - 1] != '\0')
+		{
+			dest[dest_size] = src[i];
+			dest_size++;
+			i++;
+		}
+	}
+	return (dest);
+}
---
+++ piscina/c_03/ex03/ft_strncat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 18:28:17 by franmart          #+#    #+#             */
+/*   Updated: 2022/08/17 18:36:06 by franmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	space_needed;
+	unsigned int	dest_size;
+	unsigned int	i;
+
+	dest_size = ft_strlen(dest);
+	space_needed = dest_size + ft_strlen(src) + 1;
+	i = 0;
+	if (space_needed < (sizeof(*dest) / sizeof(dest[0])))
+	{
+		while (src[i - 1] != '\0' && i < nb)
+		{
+			dest[dest_size] = src[i];
+			dest_size++;
+			i++;
+		}
+	}
+	return (dest);
+}
