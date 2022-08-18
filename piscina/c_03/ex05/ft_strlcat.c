8357ae341083aa113263825f2425190826222152
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-18 10:49:05 +0200

    Ejercicio 5 actualizado: sigue sin funcionar

--- piscina/c_03/ex05/ft_strlcat.c
+++ piscina/c_03/ex05/ft_strlcat.c
@@ -6,12 +6,9 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 09:33:12 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/18 10:46:24 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/18 10:48:13 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
 
 int	ft_strlen(char *str)
 {
@@ -40,25 +37,4 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		i++;
 	}
 	return (dest_size);
-}
-
-int	main(void)
-{
-	char	c1[30]="Hola ";
-	char	c2[]="mundooo!";
-	unsigned int	i;
-	unsigned int	j;
-
-	i = strlcat(c1, c2, 8);
-	printf("%s", c1);
-	printf("\n");
-	printf("%d\n", i);
-
-	char	c3[30]="Hola ";
-	printf("La mia:\n");
-	j = ft_strlcat(c3, c2, 8);
-	printf("%s", c3);
-	printf("\n");
-	printf("%d", j);
-	return (0);
 }
