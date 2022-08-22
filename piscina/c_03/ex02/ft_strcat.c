22a42220f8dc8a1e7acc07848ee5234a91a7b1c5
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-22 13:37:24 +0200

    Ejercicio 02 revisado

--- piscina/c_03/ex02/ft_strcat.c
+++ piscina/c_03/ex02/ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 17:52:58 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/22 12:54:30 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/22 13:20:01 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,10 @@ int	ft_strlen(char *str)
 
 char	*ft_strcat(char *dest, char *src)
 {
-	unsigned int	space_needed;
 	int				dest_size;
 	int				i;
 
 	dest_size = ft_strlen(dest);
-	space_needed = dest_size + ft_strlen(src) + 1;
 	i = 0;
 	while (src[i] != '\0')
 	{
