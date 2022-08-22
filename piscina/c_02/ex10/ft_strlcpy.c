4a32f2401497ac16e2d7630b4664f248865a570c
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-22 10:32:01 +0200

    Se sigue saliendo de la memoria

--- piscina/c_02/ex10/ft_strlcpy.c
+++ piscina/c_02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 12:36:28 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/17 17:15:46 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/22 10:31:08 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,13 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	src_len;
+	unsigned int	dest_len;
 
 	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
 	i = 0;
+	if (size > dest_len)
+		return (0);
 	while (i < size - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
@@ -38,3 +42,19 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	dest[size - 1] = '\0';
 	return (src_len);
 }
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char string[] = "Hola";
+	char string_dest[5];
+	int	dest_len;
+	
+	dest_len = ft_strlen(string_dest);
+	ft_strlcpy(string_dest, string, 7);
+	printf("%s\n", string_dest);
+	printf("%d", dest_len);
+	return (0);
+}	
