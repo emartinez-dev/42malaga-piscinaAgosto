d37bd2f0124d383a6f006067004daac969f366ab
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-24 11:29:44 +0200

    ejercicios 0 y 1 del c_05

---
+++ piscina/c_05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 11:10:32 by franmart          #+#    #+#             */
+/*   Updated: 2022/08/24 11:21:07 by franmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	while (nb > 1)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
---
+++ piscina/c_05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 11:22:01 by franmart          #+#    #+#             */
+/*   Updated: 2022/08/24 11:27:51 by franmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 2)
+		return (1);
+	result = ft_recursive_factorial(nb - 1) * nb;
+	return (result);
+}
