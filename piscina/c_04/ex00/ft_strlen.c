31f39e2795360ce028f781b8e177bcfd6837897b
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-25 12:11:50 +0200

    c04/ex00 modificado strlen

--- piscina/c_04/ex00/ft_strlen.c
+++ piscina/c_04/ex00/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 15:54:20 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/18 15:54:44 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/25 12:11:18 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while (*str != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
-		str++;
 	}
 	return (i);
 }
