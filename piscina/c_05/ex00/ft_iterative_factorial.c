24c3760cc76d85ab1d6cd90d8bf17aa6b317481c
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-29 12:52:41 +0200

    Corregidos el 0 y el 1 del c_05 que daban error con números negativos)

--- piscina/c_05/ex00/ft_iterative_factorial.c
+++ piscina/c_05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 11:10:32 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/24 11:21:07 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/26 12:53:45 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ int	ft_iterative_factorial(int nb)
 	int	result;
 
 	result = 1;
+	if (nb < 0)
+		return (0);
 	while (nb > 1)
 	{
 		result *= nb;
--- piscina/c_05/ex01/ft_recursive_factorial.c
+++ piscina/c_05/ex01/ft_recursive_factorial.c
@@ -6,14 +6,16 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 11:22:01 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/24 11:27:51 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/26 13:04:46 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
 	int	result;
-
+	
+	if (nb < 0)
+		return (0);
 	if (nb < 2)
 		return (1);
 	result = ft_recursive_factorial(nb - 1) * nb;
