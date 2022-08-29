b8e1953309c09359007bba991d1bf23023135307
Francisco Enrique Martinez Diaz <franmart@student.42malaga.com>
2022-08-29 16:52:59 +0200

    fixed time complexity

--- piscina/c_05/ex07/ft_find_next_prime.c
+++ piscina/c_05/ex07/ft_find_next_prime.c
@@ -6,32 +6,32 @@
 /*   By: franmart <franmart@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 16:49:53 by franmart          #+#    #+#             */
-/*   Updated: 2022/08/24 17:04:58 by franmart         ###   ########.fr       */
+/*   Updated: 2022/08/29 16:51:40 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb)
 {
 	int	i;
-	int	divisors;
-	int	remainder;
 
-	divisors = 0;
-	i = 1;
-	if (nb <= 0 || nb == 1)
+	i = 2;
+	if (nb < 2)
 		return (0);
-	while (divisors < 3 && i <= nb)
+	if (nb == 2147483647)
+		return (1);
+	while (i * i < nb)
 	{
-		remainder = nb % i;
-		if (remainder == 0)
-			divisors++;
+		if (nb % i == 0)
+			return (0);
 		i++;
 	}
-	return (!(divisors > 2));
+	return (1);
 }
 
 int	ft_find_next_prime(int nb)
 {
+	if (nb < 2)
+		return (2);
 	while (!ft_is_prime(nb))
 	{
 		nb++;
